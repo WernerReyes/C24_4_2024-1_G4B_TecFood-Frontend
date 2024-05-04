3b52971dcf6b7a5003b373ed0a5c232e75946fca
PROGRAMMINGEGP <guerreroerminio30@gamil.com>
2024-05-04 11:56:01 -0500

    Login User 80%

--- src/presentation/pages/public/login/pages/Login.page.tsx
+++ src/presentation/pages/public/login/pages/Login.page.tsx
@@ -1,15 +1,89 @@
-import { InputText } from "primereact/inputtext";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AuthLayout } from "../../layout";
+import { Button, InputPassword, InputText } from "@/presentation/components";
+import { registerUserDto } from "@/domain/dtos";
+import { SonnerManager } from "@/utilities";
+import { useTheme, useAuthStore } from "@/presentation/hooks";
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
 
 export const LoginPage = () => {
+  const { isDark } = useTheme();
+  const { startRegisteringUser } = useAuthStore();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    resolver: zodResolver(registerUserDto),
+  });
+
+  const handleLogin: SubmitHandler<LoginForm> = (data) => {
+    SonnerManager.success("Inicio de sesión exitoso");
+  };
+
   return (
-    <div>
-      <InputText
-        type="text"
-        placeholder="Username"
-        className="mx-auto mt-5 bg-red-600"
-      />
-    </div>
+    <AuthLayout
+      title="Inicia sesión"
+      label="¿No tienes una cuenta?"
+      link="/register"
+      labelLink="Regístrate"
+      showGoogleAuth
+      isDark={isDark}
+    >
+      <form
+        className="mt-4 flex flex-col space-y-4"
+        onSubmit={handleSubmit(handleLogin)}
+      >
+        <Controller
+          name="email"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <InputText
+              {...field}
+              label="Correo Electrónico"
+              placeholder="Ingresa tu email"
+              error={errors[field.name]?.message}
+              className="border-2 border-primary bg-transparent py-3 text-sm"
+              showAlertError
+            />
+          )}
+        />
+        <Controller
+          name="password"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <InputPassword
+              {...field}
+              label="Contraseña"
+              placeholder="Ingresa tu contraseña"
+              
+              error={errors[field.name]?.message}
+              inputClassName="border-2 border-primary text-sm py-3 bg-transparent"
+              panelClassName="dark:bg-[#1e293b] dark:text-slate-300 text-xs p-4"
+              showAlertError
+            />
+          )}
+        />
+        <Button
+          type="submit"
+          label="Iniciar Sesión"
+          className="mt-5 w-full rounded-md dark:text-slate-100"
+          disabled={Object.keys(errors).length > 0}
+        />
+
+      </form>
+    </AuthLayout>
   );
 };
 
 export default LoginPage;
+
+
--- src/presentation/pages/public/register/pages/Register.page.tsx
+++ src/presentation/pages/public/register/pages/Register.page.tsx
@@ -173,10 +173,3 @@ export const RegisterPage = () => {
 
 export default RegisterPage;
 
-
-// first_name VARCHAR(255) NOT NULL ,
-//                        last_name VARCHAR(255) NOT NULL ,
-//                        phone_number INT(9),
-//                        email VARCHAR(255) UNIQUE NOT NULL ,
-//                        password VARCHAR(255) NOT NULL ,
-//                        dni INT,
