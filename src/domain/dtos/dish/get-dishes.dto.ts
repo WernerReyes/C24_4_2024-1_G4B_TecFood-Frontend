c94407260142061a55b942d4ebddb36ca734322f
Werner Reyes <reyespilcowerner@gmail.com>
2024-05-17 13:23:43 -0500

    feat: Add search functionality to getDishesDto

--- src/domain/dtos/dish/get-dishes.dto.ts
+++ src/domain/dtos/dish/get-dishes.dto.ts
@@ -4,6 +4,7 @@ import { paginationDto } from "../common";
 export const getDishesDtoSchema = z.object({
   ...paginationDto.shape,
   idCategory: z.nullable(z.number().int().positive()),
+  search: z.nullable(z.string()),
 });
 
 export type GetDishesDto = z.infer<typeof getDishesDtoSchema>;
