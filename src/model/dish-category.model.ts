822c4237feb4cde8a9d5e75317d9567863a92de9
Werner Reyes <reyespilcowerner@gmail.com>
2024-05-17 00:56:04 -0500

    feat: Update dish category model and interfaces

--- src/model/dish-category.model.ts
+++ src/model/dish-category.model.ts
@@ -2,3 +2,15 @@ export interface DishCategory {
   id: number;
   name: string;
 }
+
+export interface GetDishCategories {
+  message: string;
+  dishCategories: DishCategory[];
+}
+
+export interface DishCategoryState extends DishCategory {}
+
+export const dishCategoryEmptyState: DishCategoryState = {
+  id: 0,
+  name: "",
+};
