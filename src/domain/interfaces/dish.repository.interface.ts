3b8c970310a7f57bf308b51f379a41a0f875057e
Werner Reyes <reyespilcowerner@gmail.com>
2024-05-16 23:44:39 -0500

    feat: Update dish repository interface to accept GetDishesDto parameter

--- src/domain/interfaces/dish.repository.interface.ts
+++ src/domain/interfaces/dish.repository.interface.ts
@@ -1,7 +1,8 @@
-import { GetDishes } from "@/model";
+import type { GetDishes } from "@/model";
+import type { GetDishesDto } from "../dtos";
 
 export interface DishRepository {
-  getAll: () => Promise<GetDishes>;
+  getAll: (getDishesDto: GetDishesDto) => Promise<GetDishes>;
   //   create: (dishOffer: CreateDishOffer) => Promise<DishOffer>;
   //   update: (dishOffer: UpdateDishOffer) => Promise<DishOffer>;
   //   delete: (id: string) => Promise<void>;
