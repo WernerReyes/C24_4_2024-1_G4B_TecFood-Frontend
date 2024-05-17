d22b353680adba6c69c2b712ec77375ecfa23eac
Werner Reyes <reyespilcowerner@gmail.com>
2024-05-16 23:44:45 -0500

    feat: Update getDishes use case to accept GetDishesDto parameter

--- src/domain/use-cases/dish/get-dishes.ts
+++ src/domain/use-cases/dish/get-dishes.ts
@@ -1,14 +1,15 @@
 import type { DishRepository } from "@/domain/interfaces";
 import type { GetDishes } from "@/model";
+import type { GetDishesDto } from "../../dtos/dish/";
 
 interface GetDishesUseCase {
-  execute(): Promise<GetDishes>;
+  execute(getDishesDto?: GetDishesDto): Promise<GetDishes>;
 }
 
 export const getDishes = (repository: DishRepository): GetDishesUseCase => {
   return {
-    async execute(): Promise<GetDishes> {
-      return await repository.getAll();
+    async execute(getDishesDto: GetDishesDto): Promise<GetDishes> {
+      return await repository.getAll(getDishesDto);
     },
   };
 };
